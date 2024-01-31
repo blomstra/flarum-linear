b3f6926eba588a9e52ae1c84939010858686710f
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2024-01-31 13:15:12 +0400

    Add sent to icon

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -5,10 +5,11 @@ import Discussion from "flarum/common/models/Discussion";
 import LinkButton from "flarum/common/components/LinkButton";
 import Button from "flarum/common/components/Button";
 import SendToLinearModal from "./components/SendToLinearModal";
+import Badge from 'flarum/common/components/Badge';
 
 app.initializers.add('blomstra/linear', () => {
   extend(DiscussionPage.prototype, 'sidebarItems', function (items) {
-    const discussion  = this.discussion;
+    const discussion:Discussion  = this.discussion;
 
     let linear = discussion.attribute('linearIssueId');
 
@@ -25,21 +26,36 @@ app.initializers.add('blomstra/linear', () => {
       'blomstra-linear',
       linear === null ?
         (
-          <Button icon="fa fa-paper-plane" class="Button" onclick={() => app.modal.show(SendToLinearModal, { discussion })}>
+          <Button icon="far fa-paper-plane" class="Button" onclick={() => app.modal.show(SendToLinearModal, { discussion })}>
             {app.translator.trans('blomstra-linear.forum.controls.send_to_linear_button')}
           </Button>
         ) : (
           <LinkButton
-            icon="fa fa-paper-plane"
+            icon="far fa-paper-plane"
             class="Button"
             href={"https://linear.app/" + lOrg + "/issue/" + lId}
             external={true}
             target="_blank">
             Open in Linear
           </LinkButton>
-  ), 100
-  )
+  ), 100);
+  });
 
+  extend(Discussion.prototype, 'badges', function (badges) {
+    const discussion = this;
+    let linear = discussion.attribute('linearIssueId');
+    let lx = linear !== null ? linear.split(':::') : 'a:::b';
+    let lOrg = lx[0];
+    let lId = lx[1];
+    const canAddToLinear = discussion.attribute('canAddToLinear');
+
+    if (canAddToLinear && linear !== null) {
+      badges.add(
+        'linear',
+        <Badge type="linear" icon="far fa-paper-plane" label={app.translator.trans('blomstra-linear.forum.badges.linear.tooltip')} />,
+        10
+      );
+    }
   });
 
 
