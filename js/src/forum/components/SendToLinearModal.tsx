e59deca5fd113bbc5f4ff92bdbf3dfae02bd8de4
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2024-02-01 09:58:59 +0400

    Add loader and do not hard refresh on issue creation

--- js/src/forum/components/SendToLinearModal.tsx
+++ js/src/forum/components/SendToLinearModal.tsx
@@ -2,6 +2,8 @@ import Modal, {IInternalModalAttrs} from 'flarum/common/components/Modal';
 import app from "flarum/forum/app";
 import Button from "flarum/common/components/Button";
 import Model from 'flarum/common/Model';
+import DiscussionPage from "flarum/forum/components/DiscussionPage";
+import Discussion from "flarum/common/models/Discussion";
 
 export default class SendToLinearModal extends Modal {
   // True by default, dictates whether the modal can be dismissed by clicking on the background or in the top right corner.
@@ -17,14 +19,14 @@ export default class SendToLinearModal extends Modal {
   tags = [];
 
   getTags(): void {
-    let tags = this.discussion !== '' ? this.discussion.tags() : [];
+    let tags = this.attrs.discussion !== '' ? this.attrs.discussion.tags() : [];
     tags.map((tag) => {
       this.tags.push(tag.data.id)
     });
   }
 
   loadData(): void {
-    this.discussion = this.attrs.discussion;
+
     this.getTags();
     m.request(
       {
@@ -106,14 +108,18 @@ export default class SendToLinearModal extends Modal {
           </div>
           <div class="Form-group">
             <Button className="Button Button--primary" type="submit">
-              Send to Linear
+              <i id="linear-submission-loading" class="fa fa-spinner fa-spin visually-hidden" aria-hidden="true"></i> Send to Linear
             </Button>
           </div>
         </div>
       </div>
     </>
     } else {
-      return <p>Loading...</p>;
+      return <>
+        <div class="Modal-body">
+          <p class="center"><i class="fa fa-spinner fa-spin" aria-hidden="true"></i> Loading...</p>
+        </div>
+      </>
     }
     // Content to show in the modal's body
 
@@ -122,7 +128,8 @@ export default class SendToLinearModal extends Modal {
   onsubmit(e) {
     e.preventDefault();
     // If your modal contains a form, you can add form processing logic here.
-
+    // Show the icon in the submit button
+    document.getElementById('linear-submission-loading').classList.remove('visually-hidden');
     m.request(
       {
         method: "POST",
@@ -134,16 +141,19 @@ export default class SendToLinearModal extends Modal {
           team: this.selectedTeam,
           priority: this.selectedPriority,
           tags: this.tags,
-          discussion: this.discussion.id(),
+          discussion: this.attrs.discussion.id(),
         }
       }
     ).then((response) => {
-      console.log(response);
+      //console.log(response);
       if (response.data.id !== undefined || response.data.id !== null) {
-        //this.discussion.attrs.linearIssueId = response.data.id;
+        this.attrs.linear = response.data.id;
+        app.current.data.discussion.data.attributes.linearIssueId = response.data.id;
+        document.getElementById('linear-submission-loading').classList.add('visually-hidden');
         m.redraw();
-        app.modal.close();
-        location.reload();
+        this.hide();
+      } else {
+        alert('Something went wrong, please try again.');
       }
     });
   }
@@ -151,8 +161,8 @@ export default class SendToLinearModal extends Modal {
   oninit(vnode) {
     super.oninit(vnode);
     this.loadData();
-     this.defaultTeam = app.forum.attribute('blomstraLinearDefaultTeamId');
-     this.selectedTeam = this.defaultTeam;
+    this.defaultTeam = app.forum.attribute('blomstraLinearDefaultTeamId');
+    this.selectedTeam = this.defaultTeam;
   }
 
 }
