c7330ff06079e6fc667c098df9e08f1497824618
Jaggy <jaggy@hey.com>
2024-02-23 17:38:10 +0800

    Remove the debug

--- src/Tags/Listeners/AssignLinearLabel.php
+++ src/Tags/Listeners/AssignLinearLabel.php
@@ -3,20 +3,11 @@
 namespace Blomstra\Linear\Tags\Listeners;
 
 use Flarum\Tags\Event\Saving;
-use Psr\Log\LoggerInterface;
 
 class AssignLinearLabel
 {
-    public function __construct(private LoggerInterface $logger)
-    {
-    }
-
     public function handle (Saving $event)
     {
-        $this->logger->info(
-            data_get($event->data, 'attributes.linearLabelId', null)
-        );
-
         $event->tag->linear_label_id = data_get($event->data, 'attributes.linearLabelId', null);
     }
 }
